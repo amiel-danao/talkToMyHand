b1888d13626359a799546d188a146ca19cc8e29c
Amiel Danao <70653874+amiel-danao@users.noreply.github.com>
2022-02-08 11:58:21 +0800

    Update index2.php

--- talkToMyHand-main/public/index2.php
+++ talkToMyHand-main/public/index2.php
@@ -23,6 +23,7 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 	<link href="https://fonts.googleapis.com/css2?family=Rowdies:wght@700&display=swap" rel="stylesheet">
+	<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
 	
 </head>
 <body>
@@ -83,6 +84,10 @@
   </div>
   
   <div id="snackbar">Correct!<i class="fas fa-check"></i></div>
+	
+<div id="dialog-message" title="Wrong answer">
+  <p>Incorrect hand gesture, please try again.</p>
+</div>
   
   <footer>
 	<label for="drawPoints">
@@ -91,7 +96,7 @@
   </footer>
   
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-  
+  <script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
   <script src="https://kit.fontawesome.com/882462feea.js" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
   
